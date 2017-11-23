87694c940bf7fea5d03d1694d906b8aed95398c1
Quan Carry <shisuipro@gmail.com>
2017-11-23 13:12:40 +0700

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -1,31 +1,40 @@
 #bin/bash
 #Transfer file through remote host
-#Topo
-
-#checking ssh
+#We are running on host B
 
 #get file from Host A
-hostA=''
-hostC=''
+hostA='192.168.1.2'
+hostC='192.168.1.4'
+
+#checking ssh
+ssh-copy-id -i ~/.ssh/id_rsa.pub $hostA
+ssh-copy-id -i ~/.ssh/id_rsa.pub $hostC
 
 #set time rotate for getting file
-settime=0
+$time=10
 
 #Path of file want to get in host A
-pathA=/usl/local/
-filename=''
+pathA='/usl/local'
+filename='test.txt'
 
 #path save in host B
-pathB=/root/Desktop
+pathB='/root/Desktop'
 
-#Getting file from host A
-scp -r hostA:/path/filename pathB
 
 #path save in host C
-pathC=
-#Putting to host C
-scp -r pathB/filename hostC:/pathC
+pathC='/usr/local'
 
 
+while [ true ]
+do
+    #Getting file from host A
+    scp -r hostA:/$pathA/$filename $pathB
+    
+    #Putting to host C
+    scp -r $pathB/$filename hostC:/$pathC
+    
+    #Rotate at second(s) time
+    sleep $time
+done
 
 
