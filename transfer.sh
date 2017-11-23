4a80dc71e390fab633df8a904c330b622ce7c695
Quan Carry <shisuipro@gmail.com>
2017-11-23 12:15:57 +0700

    transfer.sh
    a

---
+++ transfer.sh
@@ -0,0 +1,27 @@
+#bin/bash
+#Transfer file through remote host
+#Topo
+
+#checking ssh
+
+#get file from Host A
+hostA=''
+hostC=''
+
+#set time rotate for getting file
+settime=0
+
+#Path of file want to get in host A
+pathA=/usl/local/
+filename=''
+
+#path save in host B
+pathB=/root/Desktop
+
+#Getting file from host A
+scp -r hostA:/path/filename pathB
+
+#path save in host C
+pathC=
+#Putting to host C
+scp -r pathB/filename hostC:/pathC
